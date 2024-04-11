026b8702ecbab0f02f2e5f97342cd48a2311874e
triparna01 <166366017+triparna01@users.noreply.github.com>
2024-04-11 19:34:09 +0530

    Create Loader.tsx

---
+++ src/components/shared/Loader.tsx
@@ -0,0 +1,15 @@
+const Loader = () => {
+  return (
+    <div className="flex-center w-full">
+        <img
+        src="/assets/icons/loader.svg"
+        alt="loader"
+        width={10}
+        height={10}
+        />
+
+    </div>
+  )
+}
+
+export default Loader
