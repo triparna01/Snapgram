663257549683ce8ae10d9bb77a40db8d5b7b7ce0
triparna01 <166366017+triparna01@users.noreply.github.com>
2024-04-11 19:01:29 +0530

    SigninForm.tsx

---
+++ src/_auth/forms/SigninForm.tsx
@@ -0,0 +1,119 @@
+
+import { z } from "zod"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Link, useNavigate } from 'react-router-dom'
+
+import {Form, FormControl, FormField, FormItem, FormLabel,FormMessage } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { Loader } from "lucide-react"
+import { useToast } from "@/components/ui/use-toast"
+
+
+import { SigninValidation } from "@/lib/validation"
+import {useSignInAccount } from "@/lib/react-query/queriesAndMutations"
+import { useUserContext } from "@/context/AuthContext"
+
+
+const SigninForm = () => {
+  const { toast } = useToast()
+  const { checkAuthUser, isLoading: isUserLoading } = useUserContext();
+  const navigate = useNavigate();
+
+  const { mutateAsync: signInAccount, isPending} = useSignInAccount();
+  
+  // Form
+  const form = useForm<z.infer<typeof SigninValidation>>({
+    resolver: zodResolver(SigninValidation),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+ 
+  // On submit function
+  async function onSubmit(values: z.infer<typeof SigninValidation>) {
+    
+    const session = await signInAccount({
+      email: values.email,
+      password: values.password,
+    })
+
+    
+    if(!session){
+      return toast({ title: 'Sign in failed1. Please try again. '})
+    }
+
+
+    const isLoggedIn = await checkAuthUser();
+
+    
+    if(isLoggedIn){
+      form.reset();
+
+      navigate("/");
+      } else{
+      return navigate("/");  //toast({title: 'Sign in failed2. Please try again.', })
+    }
+  };
+  return (
+
+    <Form {...form}>
+     <div className="sm:w-420 flex-center flex-col">
+     <img src="/assets/images/logo.svg" alt="logo" />
+      <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">Welcome Back</h2>
+        <p className="text-light-3 small-medium md:base-regular mt-2">Log in to your account</p>
+      
+        
+     
+
+      <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5 w-full m-4">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+           <FormItem>
+             <FormLabel>Email</FormLabel>
+             <FormControl>
+               <Input type="email" className="shad-input"{...field} />
+             </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+           <FormItem>
+             <FormLabel>Password</FormLabel>
+             <FormControl>
+               <Input type="password" className="shad-input"{...field} />
+             </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="shad-button_primary">
+          {isUserLoading ? (
+            <div className="flexcenter gap-4">
+              <Loader />  Loading...
+            </div>
+          ): "Sign-in" }
+        </Button>
+
+        <p className="text-small-regular text-light-2 text-center-mt-2">
+            New user?
+            <Link to="/sign-up" className="text-primary-500
+            text-small-semibold ml-1"> Sign up </Link>
+        </p>
+      </form>
+      </div>
+    </Form>
+  
+  )
+}
+
+export default SigninForm
+
